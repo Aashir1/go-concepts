2f6a39a6a9d80ef772e07250f34d7159879121c1
Aashir Khan <aashir.khan@systemsltd.com>
2023-02-20 13:19:07 +0500

    pointers with go

---
+++ pointers.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+type Player struct {
+	health int
+}
+
+func (player *Player) takeDamageFromExplosion(dmg int){ //same as below just a syntactic sugar of below function
+	fmt.Println("player is taking damage from explosion")
+	player.health -= dmg
+}
+
+func takeDamageFromExplosion(player *Player, dmg int) {
+	fmt.Println("player is taking damage from explosion")
+	player.health -= dmg
+}
+
+func main() {
+
+	player := &Player{
+		health: 100,
+	}
+	// by defining player with '&' sign, the value of palyer is 8 byte long integer
+	// without '&' it will be simple struct
+
+	fmt.Println("before explosion: ", player)
+	player.takeDamageFromExplosion(45)
+	fmt.Println("after explosion: ", player)
+
+}
